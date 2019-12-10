50242cca3e2e3f594322d38ccdd557e51e521c74
Devendra Sharma <dsharma@afilias.info>
2019-12-10 09:14:21 +0530

    update script2

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "Ths is completed or done"
+echo "Ths is completed or done lol"
 echo "this was run automagically"
